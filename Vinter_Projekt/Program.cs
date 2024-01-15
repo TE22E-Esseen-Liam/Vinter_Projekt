88bece4f3f7f7ddcb3d001cd3dcf28e4550f1027
Liam Esseen <Liam.esseen@elev.ga.ntig.se>
2024-01-15 22:40:23 +0100

    final

--- Vinter_Projekt/Program.cs
+++ Vinter_Projekt/Program.cs
@@ -1,79 +1,98 @@
 ﻿using System;
-
+using System.Collections.Generic;
 class Program
 {
-    static void Start()    //start av game
+
+    //Create a list
+    static List<string> gameResults = new List<string>();
+
+    //start of the game
+    static void Start()
     {
+        //Welcome message
         Console.Clear();
         Console.WriteLine("Welcome to Batman vs Joker");
         Console.WriteLine("");
 
+        //get players name
         string playerName;
         Console.WriteLine("Enter your name:");
         playerName = Console.ReadLine();
 
-        Console.WriteLine($"Hello {playerName} ");
+        Console.WriteLine($"Hello {playerName}!");
 
+
+        //Start as Batman or Joker
         for(int i=0; i<1; i++)
         {
-        Console.WriteLine("Press [S] to start as the Weak Batman or [A] to start as The powerful Joker");
+        Console.WriteLine("Press [S] to start as the Batman or [A] to start as The Joker");
         }
-
+        //get players choice
         ConsoleKeyInfo key = Console.ReadKey();
         while (key.Key != ConsoleKey.S && key.Key != ConsoleKey.A)
         {
             Console.Clear();
+            Console.WriteLine("Invalid Choice! Please press [S] to start as Batman or [A] to start as Joker");
             key = Console.ReadKey();
         }
 
-        Console.Clear();  //ifall man tryckt på S, A så byts till PlayGame
-        PlayGame(key.Key);
+        Console.Clear();  //if press A, S play game
+        PlayGame(key.Key, playerName);
     }
 
+    //Method to handle game
     static void Main()
     {
-        while (true)  //when game is over this is true
+        while (true) 
         {
+
+            //Start the game
             Start();
 
+            //Restart or exit
             Console.WriteLine("Press [R] to restart the game or press any other button to exit");
             if (Console.ReadKey().Key != ConsoleKey.R)
             {
-break;
+break;                   //Break the loop
             }
 
             Console.Clear();
         }
     }
 
-    static void PlayGame(ConsoleKey playerKey)  //This is the game code
+    //Play actual game
+    static void PlayGame(ConsoleKey playerKey, string playerName)
     {
         int player1HP = 1000;  //Batman HP
         int player2HP = 1000;  //Joker HP
 
-        bool doubleDamage = false;  //True or false for DD
+        bool doubleDamage = false;  //True or false for Double Damage
 
+        //When more than 0 health display stats and how to continue
         while (player1HP > 0 && player2HP > 0)
         {
-            DisplayStats("The weak Batman", player1HP);
-            DisplayStats("The powerful Joker", player2HP);
+            DisplayStats("The Batman", player1HP);
+            DisplayStats("The Joker", player2HP);
 
+            //wait for player to press ENTER
             Console.WriteLine("Press [ENTER] to continue");
             Console.ReadLine();
 
+            //Random Damage
             int player1Damage = GenerateRandomDamage();
             int player2Damage = GenerateRandomDamage();
 
+            //Deal double damage for Joker
             if (doubleDamage)
             {
                 player2Damage *= 2; // Double damage for Player 2
-                Console.WriteLine("The powerful Joker deals double damage!");
+                Console.WriteLine("The Joker used poison and does double damage!");
                 Console.WriteLine("");
                 doubleDamage = false;
             }
 
+            //Damage ability
             int player2Ability = GenerateRandomDamage();
-
             int player1GadgetAttack = GenerateRandomDamage();
 
             // Check if Batman uses his Gadget Attack
@@ -82,17 +101,14 @@ break;
             int gadgetDamage = GenerateRandomDamage();
             player2HP -= gadgetDamage;
 
-            Console.WriteLine("The weak Batman uses his Gadget Attack and deals additional damage!");
+            Console.WriteLine("The Batman uses his Gadget Attack and deals additional damage!");
             Console.WriteLine("");
-            Console.WriteLine($"The powerful Joker takes {gadgetDamage} extra damage!");
+            Console.WriteLine($"The Joker takes {gadgetDamage} extra damage!");
 
-            DisplayStats("The weak Batman", player1HP);
-            DisplayStats("The powerful Joker", player2HP);
-  
-            Console.WriteLine("Press [ENTER] to continue");
             Console.ReadLine();
             }
 
+            //Health left
             player1HP -= player2Damage;
             player2HP -= player1Damage;
 
@@ -100,48 +116,65 @@ break;
 
             // Display damage taken in each round
             Console.WriteLine("");
-            Console.WriteLine($"The weak Batman takes {player2Damage} damage!");
-            Console.WriteLine($"The powerful Joker takes {player1Damage} damage!");
+            Console.WriteLine($"The Batman takes {player2Damage} damage!");
+            Console.WriteLine($"The Joker takes {player1Damage} damage!");
             Console.WriteLine("");
 
+
+            //Check if joker does extra damage
             if (player2Ability > 150)
             {
                 doubleDamage = true;
                 Console.WriteLine("");
-                Console.WriteLine("The powerful Joker is preparing for a double damage attack!");
+                Console.WriteLine("The Joker is preparing for a double damage attack!");
                 Console.WriteLine("");
             }
         }
-
-         DisplayResult(player1HP, player2HP);
+         //Display results of game
+         DisplayResult(player1HP, player2HP, playerName);
     }
 
+    //Display players stats
     static void DisplayStats(string player, int hp)
     {
         Console.WriteLine($"{player} {hp}HP");
     }
 
+    //Random damage between 5, 200
     static int GenerateRandomDamage()
     {
         Random generator = new Random();
         return generator.Next(5, 200);
     }
 
-    static void DisplayResult(int player1HP, int player2HP)
+    //Display the final result of the game
+    static void DisplayResult(int player1HP, int player2HP, string playerName)
     {
         Console.Clear();
 
-        if (player1HP <= 0)    //End results 
+        if (player1HP <= 0)
         {
-            Console.WriteLine("Batman lost to The powerful Joker... unfortunately the Joker was to powerful");
+            Console.WriteLine($"{playerName} lost to The Joker... unfortunately the Joker was to powerful");
         }
         else if (player2HP <= 0)
         {
-            Console.WriteLine("The Joker lost to Batman... Unlucky");
+            Console.WriteLine($"{playerName} lost to Batman... Unlucky");
         }
         else
         {
-            Console.WriteLine("It's a draw... try again");
+            Console.WriteLine($"It's a draw... try again");
         }
+    }
+
+    static void DisplayResults()
+    {
+         
+         Console.WriteLine("Game Results: ");
+         Console.Clear();
+
+         foreach (var result in gameResults)
+         {
+            Console.WriteLine(result);
+         }
     }
 }
